9632b256ef415e332400f004ab25e78e208cb1cc
Amber Smith <asmith90@elon.edu>
2014-04-21 21:58:46 -0400

    Update messageCounterPerMonth
    Updated comments.

--- messageCountPerMonth.php
+++ messageCountPerMonth.php
@@ -3,15 +3,11 @@
 	<p> jsfiddle </p> 
 	<table>
 	<?php
-	//start dates
-	//200305
-	//go through all the pastebins and see whats the first date and use that for all
+	//start date: 200305 (chose this date because it was the earliest mention of any of the pastebin websites)
 	$todaysDate = '201403';
 	$year = 2003;
 	$month = 5;
 	$display = "";
-	//$year = 2013;
-	//$month = 12;
 	
 	$date = "";
 	if($month < 10)
@@ -28,7 +24,6 @@
 		//example markmail.org/search/?q=pastebin+date%3A201004
 		$pastebin = 'jsfiddle';
 		$url = 'http://markmail.org/search/?q=' . $pastebin . '+date%3A' . $date;
-		//$url = 'http://markmail.org/search/?q=' . $pastebin . '+-list%3Acom.googlegroups.jquery-br+date%3A' . $date . '+';
 		
 		$file_string = file_get_contents($url);
 	
@@ -41,7 +36,7 @@
 		$number = substr($span_info_out, $about_substring, $length);
 		
 		//put the number and date in a row of a table
-		//if parsing and no count was return the web scraper returns span> because of parsing
+		//if parsing and no count was return the web scraper returns span> because of parsing the html 
 		if(strpos($number, 'pan>') == false)
 		{
 			$display = "<tr>" . "<td>" . $date . "</td>" . "<td>" . "0" . "</td>" . "</tr>";
